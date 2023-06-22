87c6b340b780d8350377f7b46618f670c40e947d
faustinojr3 <faustinojunior2020pro@gmail.com>
2023-06-22 11:57:34 -0400

    Configurando base e autor

---
+++ src/api/components/autor/autor.entity.ts
@@ -0,0 +1,25 @@
+
+import { IsNotEmpty, IsDateString } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('autor')
+export class Autor {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @IsNotEmpty({
+        message: 'O atributo nome não pode estar vazio'
+    })
+    @Column()
+    nome!: string;
+
+    @Column()
+    nacionalidade!: string;
+
+    @IsDateString({strict: true})
+    @Column()
+    data_nascimento!: Date;
+        
+    @Column()
+    perfil!: string;
+}
---
+++ src/api/components/base/base.controller.ts
@@ -0,0 +1,17 @@
+
+import { Request, Response } from 'express';
+
+export class BaseController {
+  
+  public index(req: Request, res: Response) {
+    res.status(200).json({ message: 'Api running....' });
+  }
+
+  public info(req: Request, res: Response) {
+    res.status(200).json({
+      name: 'API REST  - Biblioteca Online',
+      mode: 'development',
+      version: '1.0.0',
+    });
+  }
+}
